f489232e1564e12333be2af950627eab53fb1d74
dxing97 <danielxing97@gmail.com>
2018-09-21 21:42:51 -0400

    fix C99 compatability

--- main.c
+++ main.c
@@ -33,12 +33,13 @@
 // note: allocates its own memory
 unsigned char* hexstr_to_char(const char* hexstr)
 {
+    size_t i, j;
     size_t len = strlen(hexstr);
     if(len % 2 != 0)
         return NULL;
     size_t final_len = len / 2;
     unsigned char* chrs = (unsigned char*)malloc((final_len+1) * sizeof(*chrs));
-    for (size_t i=0, j=0; j<final_len; i+=2, j++)
+    for (i=0, j=0; j<final_len; i+=2, j++)
         chrs[j] = (hexstr[i] % 32 + 9) % 25 * 16 + (hexstr[i+1] % 32 + 9) % 25;
     chrs[final_len] = '\0';
     return chrs;
@@ -116,7 +117,6 @@ int main() {
 
 
     BN_CTX_free(context);
-    context = NULL;
 
     return 0;
 }
